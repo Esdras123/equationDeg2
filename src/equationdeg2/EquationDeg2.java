241d05e1ea6e803b9053ed56b1601159b24f1b17
Esdras Fandio <fandioemma@gmail.com>
2020-02-26 21:53:06 +0100

    init3

--- src/equationdeg2/EquationDeg2.java
+++ src/equationdeg2/EquationDeg2.java
@@ -22,29 +22,7 @@ public class EquationDeg2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BufferedReader buffer = null;
-        String ligne;
-        String[] tab = new String[3];
-        Double[] val = new Double[3];
-        try {
-            buffer = new BufferedReader(new FileReader(args[0]));
-            while ((ligne = buffer.readLine() != null)) {
-                tab = ligne.split(" ");
-                for(int i=0; i < tab.length; i++){
-                    val[i] = Double.valueOf(tab[i]);
-                }
-                System.out.println();
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(EquationDeg2.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(EquationDeg2.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
-    }
-
-    public static testEquation(double a, double b,){
-        
     }
     
 }
