7f9483fdb88bc9f02a70d8754dbc1038f2f13609
mbathe <mbathepaul@gmail.com>
2020-02-29 16:43:26 +0100

    modification de afficharge

---
+++ src/equationdeg2/Afficharge.java
@@ -0,0 +1,122 @@
+package equationdeg2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Afficharge {
+
+public static void main(String[] args) throws IOException {
+		
+		
+		
+		String debut="*******************************************************************************************************************************\n";
+		System.out.println(titre("Donnees de test pour le résolution de l'éqution aX^2+bX+c=0"));
+		System.out.print(separation());
+		System.out.print(creerLigne("0225","Chambre","11454848884","4444444444444444","788881547","0255488","true"));
+		System.out.print(creerLigne("Chambre","INDEX","","","","",""));
+		System.out.print(creerLigne("Chambre","INDEX","","","","",""));
+		System.out.print(creerLigne("Chambre","INDEX","","","","",""));
+		System.out.print(creerLigne("Chambre","INDEX","","","","",""));
+		System.out.print(creerLigne("Chambre","INDEX","","","","",""));
+		
+	}
+	
+	
+	public static String titre(String titre) {
+	//	System.out.println(titre.length());
+		String affiche="";
+			String debut="*************************************************************************************************************************************************************************************************************************\n";
+		
+		   StringBuilder sb = new StringBuilder();
+		   sb.append(debut);
+		   sb.append("*");
+		   sb.append(espaces(debut.length()-3));
+		   sb.append("*\n");
+		   sb.append("*");
+		  if(titre.length()%2==0) {
+			  sb.append(espaces(debut.length()/2-titre.length()/2-1));
+		  }else {
+			  sb.append(espaces(debut.length()/2-titre.length()/2-2));
+		  }
+		   sb.append(titre);
+		   sb.append(espaces(debut.length()/2-titre.length()/2-2));
+		   sb.append("*\n");
+		   sb.append("*");
+		   sb.append(espaces(debut.length()-3));
+		   sb.append("*\n");
+		   sb.append(debut);
+		   affiche+=sb.toString()+"\n";
+		   return affiche;
+		
+		
+	}
+	
+	public static String espaces(int nombre){
+		   StringBuilder sb = new StringBuilder();
+		   for(int i=0; i<nombre; i++){
+		      sb.append(" ");
+		   }
+		   return sb.toString();
+		}
+
+		public static String creerLigne(String a,String b, String c, String tol, String ra, String ro, String oracle){
+			String affiche="";
+		   StringBuilder sb = new StringBuilder();
+		   sb.append("| ");
+		   sb.append(a);
+		   sb.append(espaces(26-a.length()));
+		   
+		   sb.append("| ");
+		   sb.append(b);
+		   sb.append(espaces(26-b.length()));
+		 
+		   sb.append("| ");
+		   sb.append(c);
+		   sb.append(espaces(26-c.length()));
+		   
+		   sb.append("| ");
+		   sb.append(tol);
+		   sb.append(espaces(20-tol.length()));
+		   
+		   sb.append("| ");
+		   sb.append(ra);
+		   sb.append(espaces(50-ra.length()));
+		   
+		   sb.append("| ");
+		   sb.append(ro);
+		   sb.append(espaces(50-ro.length()));
+		   
+		   sb.append("| ");
+		   sb.append(oracle);
+		   sb.append(espaces(8-oracle.length()));
+		  
+		   affiche+=sb.toString()+"|\n";
+		   affiche+=separation();
+		   return affiche;
+		}
+		public static String separation() {
+			
+			String a="+";
+			for(int i=1 ;i<220;i++) {
+				switch(i) {
+				case 28: a+="+";
+				break;
+				case 56: a+="+";
+				break;
+				case 84: a+="+";
+				break;
+				case 106: a+="+";
+				break;
+				case 158: a+="+";
+				break;
+				case 210: a+="+";
+				break;
+				default: a+="-";
+				
+				}	
+			}
+			
+		return a+"+\n";
+		}
+		
+}
--- src/equationdeg2/EquationDeg2.java
+++ src/equationdeg2/EquationDeg2.java
@@ -16,24 +16,28 @@ public class EquationDeg2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-  
+		System.out.println(Afficharge.titre("Donnees de test pour la résolution de l'éqution aX^2+bX+c=0"));
         ArrayList<DonneeTest> donneesTest = GenerationDonnee.getDonneesTest("scennarios.txt", GenerationDonnee.genererDonneesEntree(), 10e-6, "Solver", "Test");
 
         Class solverInterface = Class.forName("equationdeg2.Solver");
         SolverInterface solv = (SolverInterface) solverInterface.newInstance();
         int total = 0, totFalse = 0;
-        System.out.println("TolÃ©rance:  " + donneesTest.get(0).tolerance);
+       
+        System.out.println( Afficharge.separation());
+        System.out.println(Afficharge.creerLigne("a", "b", "c", "Tolérance", "Résultat attendu", "Résultat obtenu", "Oracle"));
         for (int i=0; i<donneesTest.size(); i++){
             ArrayList<Double> results = solv.resolve(donneesTest.get(i).donneeEntree.a, donneesTest.get(i).donneeEntree.b, donneesTest.get(i).donneeEntree.c);
             boolean decision = Test.verify(donneesTest.get(i).donneeEntree, results, donneesTest.get(i).resultatAttendu, donneesTest.get(i).tolerance);
-            System.out.println(donneesTest.get(i).toString() + " RO: " + results.toString() + " Oracle: " + decision);
-            System.out.println("\n");
+          
+           System.out.println(Afficharge.creerLigne( Double.toString(donneesTest.get(i).donneeEntree.a), Double.toString(donneesTest.get(i).donneeEntree.b),Double.toString(donneesTest.get(i).donneeEntree.c) ,Double.toString(donneesTest.get(0).tolerance) , donneesTest.get(i).resultatAttendu.toString(), results.toString(), Boolean.toString(decision)));
             if (!decision)
                 totFalse += 1;
             total += 1;
+           
         }
-        System.out.println("\n\nTotal Tests RatÃ©s: "+totFalse+"/"+" "+total);
-        System.out.println("Total Tests RÃ©ussis: "+(total - totFalse)+"/"+" "+total);
+        
+    	System.out.println(Afficharge.titre("Total Tests Ratés: "+totFalse+"/"+" "+total + "             Total Tests Réussis: "+(total - totFalse)+"/"+" "+total));
+        
 
     }
 }
